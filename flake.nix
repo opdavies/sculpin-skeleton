143009ab601bfc3ef7b07bad81ac66fec743e43a
Oliver Davies <oliver@oliverdavies.uk>
2024-01-09 17:40:55 +0000

    Add Nix Flake for local development

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "oliverdavies.uk-sculpin";
+
+  inputs = {
+    devshell.url = "github:numtide/devshell";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ inputs.devshell.flakeModule ];
+
+      systems = [ "x86_64-linux" ];
+
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devshells.default = {
+          packages = with pkgs; [ "php81" "php81Packages.composer" ];
+        };
+      };
+    };
+}
